f0be77c03d4d9515fbf0aeae5ab385632893438b
Ashutosh Sajan <44765540+AshutoshSajan@users.noreply.github.com>
2023-02-25 07:50:22 +0000

    fix message

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function(request, response) {
-  response.status(200).send({hello: "world"});
+app.get('/', function (request, response) {
+  response.status(200).send({ hello: 'world' });
 });
 
-const listener = app.listen(process.env.PORT || 3000, function() {
-  console.log('Your app is listening on port ' + process.env.PORT || 3000);
+const listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + (process.env.PORT || 3000));
 });
